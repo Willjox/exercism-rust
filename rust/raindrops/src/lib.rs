b733f720526cf8801f46633f57ebb99c976f8b7f
willjox <william.jox@gmail.com>
2018-05-17 12:50:10 +0200

    Raindrops

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,17 @@
+
 pub fn raindrops(n: usize) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut drops = String::from("");
+    if n%3 == 0 {
+        drops.push_str("Pling");
+    }
+    if n%5 == 0 {
+        drops.push_str("Plang");
+    }
+    if n%7 == 0 {
+        drops.push_str("Plong");
+    }
+    if drops.len() == 0 {
+        drops = n.to_string();
+    } 
+    return drops;
 }
